681b7c890819b7c86a3fce495eac5cf3b27cd856
Hashir <Hashiraslam2015@gmail.com>
2024-03-20 11:17:34 +0530

    Incomplete feature

--- js/index.js
+++ js/index.js
@@ -8,11 +8,22 @@ gridContainer.setAttribute("id", "gridContainer");
 container.appendChild(gridContainer);
 
 function createGrid(row, col){
+    gridContainer.innerHTML = "";
+
+    let containerWidth = container.offsetWidth;
+    let containerHeight = container.offsetHeight;
+
+    let squareSize = Math.min(containerWidth/col, containerHeight/row);
+
+    gridContainer.style.width = squareSize*col + "px";
+    gridContainer.style.height = squareSize*row + "px";
 
     for (let i = 0; i<row; i++){
         for (let j = 0; j<col; j++){
             let square = document.createElement("div");
             square.classList.add("square");
+            square.style.width = squareSize + "px";
+            square.style.height = squareSize + "px";
             gridContainer.appendChild(square);
             square.addEventListener("mouseover", () => {
                 square.classList.add("grid-hover");
@@ -28,3 +39,11 @@ function createGrid(row, col){
 createGrid(16, 16);
 
 let gridBtn = document.querySelector("#changeValue");
+gridBtn.addEventListener("click", () => {
+    let number = Number(prompt("Enter the preferred number of rows and col"))
+    if (!isNaN(number) && number>0){
+        createGrid(number, number);
+    }else{
+        alert("Enter a valid number");
+    }
+});
